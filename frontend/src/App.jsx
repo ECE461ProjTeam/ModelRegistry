c059f33eccea3b3aa0cd4303dbd3c3f0d1578134
Vatsal Sanjeevkum Dudhaiya <vdudhaiy@purdue.edu>
2025-10-28 12:58:02 -0400

    Empty System Health Dashboard page (#23)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,17 @@
 import React from "react";
 import UserInput from "./components/UserInput.jsx";
+import SystemHealthDashboard from "./components/SystemHealthDashboard.jsx";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <UserInput />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<UserInput />} />
+        <Route path="/health" element={<SystemHealthDashboard/>} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
