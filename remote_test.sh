97764c2ebe4fb4c2c11288f33d40560c4178a6cf
Mikhail Golovenchits <golovenchitc@gmail.com>
2025-10-29 12:27:10 -0400

    rate endpoint

---
+++ remote_test.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+function clean {
+  curl -X DELETE 'http://model-registry.us-east-2.elasticbeanstalk.com/reset' -H "X-Authorization: $stripped_string"
+}
+
+function getall {
+  curl -X POST 'http://model-registry.us-east-2.elasticbeanstalk.com/artifacts' \
+  -H "X-Authorization: $stripped_string" \
+  -H 'Content-Type: application/json' \
+  -d '{"name": "*", "types": []}'
+}
+
+response=$(curl -X PUT http://model-registry.us-east-2.elasticbeanstalk.com/authenticate \
+  -H "Content-Type: application/json" \
+  -d '{
+        "user": {
+            "name": "ece30861defaultadminuser",
+            "is_admin": true
+        },
+        "secret": {
+            "password": "correcthorsebatterystaple123(!__+@**(A'\''\"`;DROP TABLE packages;"
+        }
+      }')
+
+stripped_string="${response//\"/}"
+
+echo $stripped_string
+
+clean
+
+response=$(curl -X POST 'http://model-registry.us-east-2.elasticbeanstalk.com/artifact/model' \
+  -H "X-Authorization: $stripped_string" \
+  -H 'Content-Type: application/json' \
+  -d '{"url": "https://huggingface.co/google-bert/bert-base-uncased"}')
+
+bert_id=$(echo $response | jq -r '.metadata.id')
+echo "BERT ID: $bert_id"
+
+curl -X POST 'http://model-registry.us-east-2.elasticbeanstalk.com/artifact/dataset' \
+  -H "X-Authorization: $stripped_string" \
+  -H 'Content-Type: application/json' \
+  -d '{ "name": "bookcorpus", "url": "https://huggingface.co/datasets/bookcorpus/bookcorpus"}'
+
+
+curl -X PUT "http://model-registry.us-east-2.elasticbeanstalk.com/artifacts/model/$bert_id" \
+  -H "X-Authorization: $stripped_string" \
+  -H 'Content-Type: application/json' \
+  -d '{
+      "metadata": {
+        "name": "string",
+        "id": "48472749248",
+        "type": "model"
+      },
+      "data": {
+        "url": "https://huggingface.co/openai/whisper-tiny/tree/main",
+        "download_url": "https://ec2-10-121-34-12/download/whisper-tiny"
+      }
+}'
+
+getall
+
