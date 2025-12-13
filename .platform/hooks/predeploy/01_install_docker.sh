bff87bacb3be6b590684921e5a83d662cd8c6525
Vatsal Sanjeevkum Dudhaiya <vdudhaiy@purdue.edu>
2025-12-13 18:20:30 -0500

    docker sandbox predeploy

---
+++ .platform/hooks/predeploy/01_install_docker.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Install Docker if not present
+if ! command -v docker &> /dev/null
+then
+    amazon-linux-extras install docker -y
+    service docker start
+    usermod -a -G docker ec2-user
+fi
